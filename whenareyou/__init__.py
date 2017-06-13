4446728d041f6aa286c8a79eb4b0e4f0d46f7e1f
Udayan Tandon <udayan12167@iiitd.ac.in>
2017-06-13 17:29:07 +0530

    Add airport timezone method

--- whenareyou/__init__.py
+++ whenareyou/__init__.py
@@ -1,12 +1,27 @@
+import os
 from functools import lru_cache
 
 import requests
+import csv
 from urllib.parse import quote_plus
 from pytz import timezone
 
 
 LONG_LAT_URL = ('https://maps.googleapis.com/maps/api/geocode/json?address={0}'
                 '&sensor=false')
+
+airports_csv_path = os.path.join(os.path.dirname(__file__), 'airports.csv')
+
+airports_dict = {}
+
+with open(airports_csv_path) as csvfile:
+    airports_reader = csv.DictReader(
+        csvfile,
+        fieldnames=['id', 'name', 'city', 'country', 'iata', 'icao', 'lat',
+                    'lng', 'alt', 'tz', 'dst', 'tz_olson'],
+        restkey='info')
+    for row in airports_reader:
+        airports_dict[row['iata']] = row
 
 
 @lru_cache(None)
@@ -36,6 +51,9 @@ def whenareyou(address):
     return get_tz(latlong['lat'], latlong['lng'])
 
 
+def whenareyou_apt(airport):
+    return timezone(airports_dict[airport]['tz_olson'])
+
+
 if __name__ == '__main__':
     main()
-
